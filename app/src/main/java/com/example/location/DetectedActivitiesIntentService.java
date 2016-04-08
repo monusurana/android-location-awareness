770dfe8c7c11f41d7714e42ef81ec777db5fa87f
Monu Surana <monu.surana@gmail.com>
2016-04-08 16:47:40 -0700

    Added Activity Recognition to the application

---
+++ app/src/main/java/com/example/location/DetectedActivitiesIntentService.java
@@ -0,0 +1,37 @@
+package com.example.location;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by msurana on 4/8/2016.
+ */
+public class DetectedActivitiesIntentService extends IntentService {
+
+    public static final String TAG = "detection_is";
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+        Intent localIntent = new Intent(Constants.BROADCAST_ACTION);
+
+        ArrayList<DetectedActivity> detectedActivities = (ArrayList) result.getProbableActivities();
+
+        Log.i(TAG, "activities detected");
+
+        // Broadcast the list of detected activities.
+        localIntent.putExtra(Constants.ACTIVITY_EXTRA, detectedActivities);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(localIntent);
+    }
+}
