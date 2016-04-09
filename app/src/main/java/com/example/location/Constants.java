f46a67b54ed8af54826ad11f3ba7fe0e6864bca3
Monu Surana <monu.surana@gmail.com>
2016-04-09 15:43:25 -0700

    Implemented Geofence sample code

--- app/src/main/java/com/example/location/Constants.java
+++ app/src/main/java/com/example/location/Constants.java
@@ -1,7 +1,7 @@
 package com.example.location;
 
-
-import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.maps.model.LatLng;
+import java.util.HashMap;
 
 /**
  * Constants used in this sample.
@@ -17,12 +17,9 @@ public final class Constants {
 
     public static final String ACTIVITY_EXTRA = PACKAGE_NAME + ".ACTIVITY_EXTRA";
 
-    public static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES";
+    public static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES_NAME";
 
-    public static final String ACTIVITY_UPDATES_REQUESTED_KEY = PACKAGE_NAME +
-            ".ACTIVITY_UPDATES_REQUESTED";
-
-    public static final String DETECTED_ACTIVITIES = PACKAGE_NAME + ".DETECTED_ACTIVITIES";
+    public static final String GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
 
     /**
      * The desired time between activity detections. Larger values result in fewer activity
@@ -32,17 +29,33 @@ public final class Constants {
      */
     public static final long DETECTION_INTERVAL_IN_MILLISECONDS = 0;
 
+
     /**
-     * List of DetectedActivity types that we monitor in this sample.
+     * Used to set an expiration time for a geofence. After this amount of time Location Services
+     * stops tracking the geofence.
      */
-    protected static final int[] MONITORED_ACTIVITIES = {
-            DetectedActivity.STILL,
-            DetectedActivity.ON_FOOT,
-            DetectedActivity.WALKING,
-            DetectedActivity.RUNNING,
-            DetectedActivity.ON_BICYCLE,
-            DetectedActivity.IN_VEHICLE,
-            DetectedActivity.TILTING,
-            DetectedActivity.UNKNOWN
-    };
+    public static final long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+
+    /**
+     * For this sample, geofences expire after twelve hours.
+     */
+    public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS =
+            GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+    //public static final float GEOFENCE_RADIUS_IN_METERS = 1609; // 1 mile, 1.6 km
+    public static final float GEOFENCE_RADIUS_IN_METERS = 1609; // 1 mile, 1.6 km
+
+    /**
+     * Map for storing information about airports in the San Francisco bay area.
+     */
+    public static final HashMap<String, LatLng> BAY_AREA_LANDMARKS = new HashMap<String, LatLng>();
+    static {
+        // San Francisco International Airport.
+        BAY_AREA_LANDMARKS.put("SFO", new LatLng(37.621313, -122.378955));
+
+        // Googleplex.
+        BAY_AREA_LANDMARKS.put("Google", new LatLng(37.422611,-122.0840577));
+
+        // Test
+        BAY_AREA_LANDMARKS.put("Home", new LatLng(32.9088168,-117.1488742));
+    }
 }
